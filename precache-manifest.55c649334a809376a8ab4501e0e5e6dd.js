5056a848b663237004be9c15585e83e3a1d1d826
Chakraborty <Sandeepan.Chakraborty@Honeywell.com>
2020-04-24 19:58:21 +0530

    Updates

---
+++ precache-manifest.55c649334a809376a8ab4501e0e5e6dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4a29e4c6377645997b56a63b9f81538",
+    "url": "/demo-project/index.html"
+  },
+  {
+    "revision": "9a973c60b5a7af0d8a84",
+    "url": "/demo-project/static/css/2.dd646c47.chunk.css"
+  },
+  {
+    "revision": "94f555a23c1518b6b1e4",
+    "url": "/demo-project/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9a973c60b5a7af0d8a84",
+    "url": "/demo-project/static/js/2.6c1b93fe.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/demo-project/static/js/2.6c1b93fe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "94f555a23c1518b6b1e4",
+    "url": "/demo-project/static/js/main.846549f4.chunk.js"
+  },
+  {
+    "revision": "8d6c20b8624e8562f428",
+    "url": "/demo-project/static/js/runtime-main.35345bf8.js"
+  }
+]);
